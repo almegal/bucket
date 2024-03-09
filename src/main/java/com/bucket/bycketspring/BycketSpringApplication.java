23068a92b54ea5c1dc423c7396a45358484d3c20
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-09 14:26:48 +0300

    initial commit

---
+++ src/main/java/com/bucket/bycketspring/BycketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bucket.bycketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BycketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BycketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bucket/bycketspring/BycketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bucket.bycketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BycketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
