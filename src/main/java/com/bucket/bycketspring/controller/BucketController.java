7a6ef042cb44a54129ec1b1d2f8da59d1e1d0ef9
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-09 15:06:31 +0300

    added controller

---
+++ src/main/java/com/bucket/bycketspring/controller/BucketController.java
@@ -0,0 +1,20 @@
+package com.bucket.bycketspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BucketController {
+
+    @GetMapping(path="/add")
+    public String add(){
+        return "";
+    }
+
+    @GetMapping(path="/get")
+    public String get(){
+        return "";
+    }
+}
